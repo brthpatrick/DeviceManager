d7237ec20c0fef7bce16ba00eea14313acb424c0
Patrick B. <barth.patrick-laszlo.23@stud.umfst.ro>
2026-04-06 14:06:36 +0300

    Add landing page

---
+++ DeviceManager.UI/src/app/components/home/home.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth';
+
+@Component({
+  selector: 'app-home',
+  imports: [CommonModule, RouterLink],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+  constructor(public authService: AuthService) {}
+}
